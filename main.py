a353702bc51dbd5c88e86d53f3eb17d96a897f8d
Bogdan <romanenko_b00@mail.ru>
2021-06-10 21:45:49 +0300

    edit main

--- main.py
+++ main.py
@@ -2,4 +2,13 @@ from helpers import get_book_source
 
 """Вызовите функцию get_book_source, которая принимает возможное название книги и количество источников"""
 
-get_book_source("Python", 3)
+# get_book_source("Python", 3)
+
+book = str(input("Введи книгу: "))
+count = int(input("Введи количество источников: "))
+print(" ")
+
+get_book_source(book, count)
+
+print(" ")
+input("Введи любой символ для выхода: ")
